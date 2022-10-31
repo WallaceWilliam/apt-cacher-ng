f8f943c7060425e0c037d7c7a7ab45cc37b32554
WallaceWilliam <volovik@media5.com>
2022-10-31 16:31:19 +0300

    b

--- apt_update.sh
+++ apt_update.sh
@@ -6,4 +6,4 @@
 
 echo "Acquire::http { Proxy \"http://${PROXY_HOST}:3142\"; };" | sudo tee /etc/apt/apt.conf.d/00_proxy
 
-sudo find /etc/apt/sources.list /etc/apt/sources.list.d/ -type f -exec sed -e 's|^\([^#]* .*\)\(https://\)|\1http://https///|g' -i {} \;
+sudo find /etc/apt/sources.list /etc/apt/sources.list.d/ -type f -exec sed -e 's|^\([^#]* .*\)\(https://\)|\1http://HTTPS///|g' -i {} \;
